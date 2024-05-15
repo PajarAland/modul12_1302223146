c24274735253f03d692f581de4f105e279adbe8a
PajarAland <raymonto08@gmail.com>
2024-05-15 07:51:48 +0700

    first commit

---
+++ modul12_1302223146/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml;
+
+namespace modul12_1302223146
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            label1.Text = Convert.ToString(CariNilaiPangkat(a,b)); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static int CariNilaiPangkat(int a, int b)
+        {
+            if (b < 0)
+            {
+                return -1;
+            }else if ( b == 0)
+            {
+                return 1;
+            }else if (b >= 10 && a >= 100)
+            {
+                return -2;
+            }
+            else
+            {
+                try
+                {
+                    checked
+                    {
+                        int pangkat = 1;
+                        for (int i = 0; i < b; i++)
+                        {
+                            pangkat *= a;
+                        }
+                        return pangkat;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    return -3;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ modul12_1302223146Tests/Form1Tests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using modul12_1302223146;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul12_1302223146.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void TESTi()
+        {
+            Assert.AreEqual(1, Form1.CariNilaiPangkat(2, 0));
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TESTii()
+        {
+            Assert.AreEqual(-1, Form1.CariNilaiPangkat(12, -1));
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TESTiii()
+        {
+            Assert.AreEqual(-2, Form1.CariNilaiPangkat(101, 11));
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TESTiv()
+        {
+            Assert.AreEqual(-3, Form1.CariNilaiPangkat(20, 500));
+            //Assert.Fail();
+        }
+    }
+}
